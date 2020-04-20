bc44de2c36de5238c78b002a934c3cc99c79ab9e
houlei930122 <895879704@qq.com>
2020-04-20 22:46:46 +0800

    提交2020/4/20

--- node/node_study/testapi/http.js
+++ node/node_study/testapi/http.js
@@ -3,6 +3,6 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
     console.log('there is a request');
-    response.end('a response from server');
+    response.end('a response from server111');
 });
 server.listen(3000);
